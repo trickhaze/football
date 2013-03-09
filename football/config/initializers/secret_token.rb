21f947e0bf982ace7b5acba0c68af8fce93bda25
trickhaze <perplehays@gmail.com>
2013-03-09 15:54:52 -0700

    init

---
+++ football/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Football::Application.config.secret_token = '1ac76c0c56b1946363002ae897c16ca74be08d6cb8ca3d377a5ac3ac7731ec4a4588b61fba736ef1db70280f14404c33fd958033e2ec6dd4471bf6afbb73609a'
